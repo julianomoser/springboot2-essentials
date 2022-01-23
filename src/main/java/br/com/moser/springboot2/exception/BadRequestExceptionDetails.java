37b3b4bc9eae649d819fb7afc0b353c5d37f2a24
Juliano Moser <juliano.moserjj@gmail.com>
2022-01-23 19:29:45 -0300

    Handler Global

---
+++ src/main/java/br/com/moser/springboot2/exception/BadRequestExceptionDetails.java
@@ -0,0 +1,5 @@
+package br.com.moser.springboot2.exception;/**
+ * @author Juliano Moser
+ */
+ public class BadRequestExceptionDetails {
+}
---
+++ src/main/java/br/com/moser/springboot2/handler/RestExceptionHandler.java
@@ -0,0 +1,5 @@
+package br.com.moser.springboot2.handler;/**
+ * @author Juliano Moser
+ */
+ public class RestExceptionHandler {
+}
