b26fdd09fe581eeb1ab3b94c9265587333c9f20c
Juliano Moser <juliano.moserjj@gmail.com>
2022-01-24 01:02:15 -0300

    RestTemplate POST

--- src/main/java/br/com/moser/springboot2/client/SpringClient.java
+++ src/main/java/br/com/moser/springboot2/client/SpringClient.java
@@ -7,8 +7,7 @@ package br.com.moser.springboot2.client;
 import br.com.moser.springboot2.domain.Vinyl;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.Arrays;
@@ -22,16 +21,33 @@ public class SpringClient {
         log.info(entity);
 
         Vinyl object = new RestTemplate().getForObject("http://localhost:8080/vinyls/{id}", Vinyl.class, 2);
-
         log.info(object);
 
         Vinyl[] vinyls = new RestTemplate().getForObject("http://localhost:8080/vinyls/all", Vinyl[].class);
-
         log.info(Arrays.toString(vinyls));
 
-        ResponseEntity<List<Vinyl>> exchange = new RestTemplate().exchange("http://localhost:8080/vinyls/all", HttpMethod.GET, null,
+        ResponseEntity<List<Vinyl>> exchange = new RestTemplate().exchange("http://localhost:8080/vinyls/all",
+                HttpMethod.GET,
+                null,
                 new ParameterizedTypeReference<>() {});
-
         log.info(exchange.getBody());
+
+//        Vinyl devinTownsend = Vinyl.builder().name("Devin Townsend").build();
+//        Vinyl devinTownsendSaved = new RestTemplate().postForObject("http://localhost:8080/vinyls/", devinTownsend, Vinyl.class);
+//        log.info("Saved vinyl {}", devinTownsendSaved);
+
+        Vinyl jackGardiner = Vinyl.builder().name("Jack Gardiner").build();
+        ResponseEntity<Vinyl> jackGardinerSaved = new RestTemplate().exchange("http://localhost:8080/vinyls/",
+                HttpMethod.POST,
+                new HttpEntity<>(jackGardiner, createJsonHeader()),
+                Vinyl.class);
+        log.info("Saved vinyl {}", jackGardinerSaved);
+
+    }
+
+    private static HttpHeaders createJsonHeader() {
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setContentType(MediaType.APPLICATION_JSON);
+        return httpHeaders;
     }
 }
